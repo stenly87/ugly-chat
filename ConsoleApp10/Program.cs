810bca727b684a4fb42d12b00e3ee18c1e37a28c
ppk <ppk_student@hotmail.com>
2021-10-12 09:06:50 +1000

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = 
+                new TcpListener(IPAddress.Parse("192.168.1.11"), 6666);
+            listener.Start();
+
+            Thread thread = new Thread(Listen);
+            thread.Start(listener);
+
+            Console.ReadLine();
+        }
+
+        static List<TcpClient> clients = new List<TcpClient>();
+
+        static void Listen(object p)
+        {
+            TcpListener listener = (TcpListener)p;
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                clients.Add(client);
+                Thread thread = new Thread(ProxyMessages);
+                thread.Start(client);
+            }
+        }
+
+        static void ProxyMessages(object p)
+        {
+            TcpClient client = (TcpClient)p;
+            string message = null;
+            var stream = client.GetStream();
+            var br = new BinaryReader(stream);
+            while (true)
+            {
+                if (stream.DataAvailable)
+                {
+                    message = br.ReadString();
+                    SendMessage(message, client);
+                }
+            }
+        }
+
+        static void SendMessage(string message, TcpClient from)
+        {
+            for (int i = 0; i < clients.Count; i++)
+            {
+                if (clients[i] == from)
+                    continue;
+                try
+                {
+                    var bw = new BinaryWriter(clients[i].GetStream());
+                    bw.Write(message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    clients.Remove(clients[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("192.168.1.11", 6666);
+            Thread thread = new Thread(Listen);
+            thread.Start(client);
+            var stream = client.GetStream();
+            using (var bw = new BinaryWriter(stream))
+                while (true)
+                {
+                    string message = Console.ReadLine();
+                    bw.Write(message);
+                }
+        }
+
+        static void Listen(object p)
+        {
+            TcpClient client = (TcpClient)p;
+            var stream = client.GetStream();
+            using (var br = new BinaryReader(stream))
+            {
+                while (true)
+                {
+                    string message = br.ReadString();
+                    Console.WriteLine(message);
+                }
+            }
+        }
+    }
+}
